ce612187a9774be22d25a83b7949f23e451c8296
josephdicdican <joseph.dicdican@gmail.com>
2020-01-17 11:15:51 +0800

    Create Pagination.js

---
+++ Pagination.js
@@ -0,0 +1,14 @@
+const Pagination = {
+    page: 1,
+    total: 0,
+    from: null,
+    to: null,
+    current_page: null,
+    last_page: null,
+    next_page_url: null,
+    prev_page_url: null,
+    data: [],
+    per_page: 100,
+}
+
+export default Pagination
